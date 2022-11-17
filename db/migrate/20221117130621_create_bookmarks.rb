c1cf37b5a1da1211680e1d8e5540a20c6c256e5f
Marine Bisson <marinemaute@free.fr>
2022-11-17 14:11:59 +0100

    Exercise 2 Rails Watch List - all set + correction migrations OK - ready for controllers and views

--- db/migrate/20221117130621_create_bookmarks.rb
+++ db/migrate/20221117130621_create_bookmarks.rb
@@ -1,6 +1,9 @@
 class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
+      t.string :comment
+      t.integer :movie_id
+      t.integer :list_id
 
       t.timestamps
     end
